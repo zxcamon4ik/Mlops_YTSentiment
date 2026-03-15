c0637c691814f5c9a600578d14c362b97e65567a
zxcamo4ik <them0tluckyperson@proton.me>
2026-03-15 02:54:04 +0100

    Pipelines base

---
+++ README.md
@@ -0,0 +1,7 @@
+```zsh
+conda env create -f environment.yml
+```
+
+```zsh
+conda activate YTsentimentAnalyzer
+```
---
+++ notebooks/setup_up.py
@@ -0,0 +1,4 @@
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv(Path(__file__).resolve().parents[1] / ".env")
